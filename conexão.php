fa7859c1b9ae068db0aba164d1b4a00fa468cd94
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-15 20:44:00 -0300

    ;

---
+++ conexão.php
@@ -0,0 +1,9 @@
+<?php
+$servidor = "localhost";
+$user = "root";
+$senha = "root";
+$banco = "bd_sistema";
+
+$conexao = new mysqli($servidor, $user, $password, $banco);
+?>
+
---
+++ insert_user.php
@@ -0,0 +1,16 @@
+<?php
+//recebendo dados do formulário
+
+$nome = $_POST['nome'];
+$setor = $_POST['setor'];
+$login = $_POST['login'];
+$senha = $_POST['senha'];
+
+include 'conexão.php';
+
+$insert = "INSERT INTO tb_user VALUES (NULL, '$nome','$setor','$login','$senha')";
+
+$query = mysqli_query($conexão, $insert);
+
+echo 'inserido com sucesso'
+?>
